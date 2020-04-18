91e1481afad18801843d1f88570d2698f63510f1
Datseris <datseris.george@gmail.com>
2020-04-18 12:42:16 +0200

    change struct to mutalbe with normal fields

--- src/billiards/defs_animating.jl
+++ src/billiards/defs_animating.jl
@@ -1,11 +1,11 @@
-struct ParticleObservable
+mutable struct ParticleObservable
     # Numbers necessary for simulation
     # TODO: do they have to be Observables?
-    i::Observable{Int}
-    tmin::Observable{Float32}
-    t::Observable{Float32}
-    n::Observable{Int}
-    T::Observable{Float32}
+    i::Int
+    tmin::Float32
+    t::Float32
+    n::Int
+    T::Float32
     # Fields used in plotting
     tail::Observable{Vector{Point2f0}}
     pos::Observable{Point2f0}
@@ -15,21 +15,22 @@ end
 function ParticleObservable(p, bd, n) # initializer
     i, tmin::Float32, cp = next_collision(p, bd)
     ξ = sφ = 0f0 # TODO: Use boundary map on cp
-    ParticleObservable(Observable.((
-        i, tmin, 0f0, 0, 0f0, [Point2f0(p.pos) for i in 1:n],
+    ParticleObservable(i, tmin, 0f0, 0, 0f0,
+        Observable.((
+         [Point2f0(p.pos) for i in 1:n],
         Point2f0(p.pos), Point2f0(p.vel), Point2f0(ξ, sφ)
     ))...)
 end
 const ParObs = ParticleObservable
 
 function animstep!(p, bd, dt, parobs, updateplot = true)
-    if parobs.t[] + dt - parobs.tmin[] > 0
-        rt = parobs.tmin[] - parobs.t[] # remaining time
+    if parobs.t + dt - parobs.tmin > 0
+        rt = parobs.tmin - parobs.t # remaining time
         animbounce!(p, bd, rt, parobs, updateplot)
     else
         propagate!(p, dt)
-        parobs.t[] += dt
-        parobs.T[] += dt
+        parobs.t += dt
+        parobs.T += dt
         popfirst!(parobs.tail[])
         push!(parobs.tail[], p.pos)
         if updateplot
@@ -43,13 +44,14 @@ end
 
 function animbounce!(p, bd, rt, parobs, updateplot = true)
     propagate!(p, rt)
-    DynamicalBilliards._correct_pos!(p, bd[parobs.i[]])
-    DynamicalBilliards.resolvecollision!(p, bd[parobs.i[]])
+    DynamicalBilliards._correct_pos!(p, bd[parobs.i])
+    DynamicalBilliards.resolvecollision!(p, bd[parobs.i])
     i, tmin::Float32, = next_collision(p, bd)
-    parobs.i[] = i
-    parobs.tmin[] = tmin
-    parobs.t[] = 0f0
-    parobs.T[] += rt
+    parobs.i = i
+    parobs.tmin = tmin
+    parobs.t = 0f0
+    parobs.T += rt
+    parobs.n += 1
     popfirst!(parobs.tail[])
     push!(parobs.tail[], p.pos)
     if updateplot
