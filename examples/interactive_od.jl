41a5c6b8822a855d7b14be6ef065323ee85f5424
Datseris <datseris.george@gmail.com>
2020-04-26 16:33:47 +0200

    new docs

--- examples/interactive_od.jl
+++ examples/interactive_od.jl
@@ -7,7 +7,7 @@ systems = [(Systems.logistic(), 2.5, 4.0, "r", "logistic map"),
            (Systems.henon(), 0.8, 1.4, "a", "Hénon map"),
            (Systems.standardmap(), 0.0, 1.2, "k", "standard map")]
 
-ds, p_min, p_max, parname, t = systems[1]
+ds, p_min, p_max, parname, t = systems[3]
 t = "orbit diagram for the "*t
 
 oddata = interactive_orbitdiagram(ds, p_index, p_min, p_max, i;
