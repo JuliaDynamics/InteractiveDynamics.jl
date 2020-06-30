4a3876e473890d4bc7d5cfa1eeb541c41607b037
Sebastian Micluța-Câmpeanu <31181429+SebastianM-C@users.noreply.github.com>
2020-07-01 02:21:27 +0300

    [WIP] Update trajectory_highlighter (#8)
    * Clean up
    
    - Fix UndefVarError for color
    - get_series_idx now returns the adjusted index
    
    * Fix histogram index
    
    * small updates
    
    * bump version
    
    Co-authored-by: Datseris <datseris.george@gmail.com>

--- src/numericdata/trajectory_highlighter.jl
+++ src/numericdata/trajectory_highlighter.jl
@@ -8,15 +8,17 @@ const DEFAULT_α = 0.01
 """
     trajectory_highlighter(datasets, vals; kwargs...)
 Open an interactive application for highlighting specific datasets
-and properties of these datasets. `datasets` is a vector of _anything_ from
-**DynamicalSystems.jl** that supports `plot_dataset` (currently `Dataset`
-or `Matrix`). Each dataset corresponds to a specific value from `vals`
+and properties of these datasets. `datasets` is a vector of `Dataset` from
+**DynamicalSystems.jl**. Each dataset corresponds to a specific value from `vals`
 (a `Vector{<:Real}`). The value of `vals` gives each dataset
 a specific color based on a colormap.
 
 The application is composed of two scenes: the left scene plots the datasets,
 while the right scene plots the histogram of the `vals`. The function
 returns the two scenes `data_scene, hist_scene`.
+
+Two dimensional datasets are plotted as scatter plots (and are assumed to be discrete
+in nature), while three dimensional are plotted as lines (and are assummed continuous).
 
 ## Interaction
 Clicking on a bin of the histogram plot will "highlight" all data
@@ -81,7 +83,7 @@ according to a colormap.
 function plot_histogram(hist::StatsBase.Histogram, cmap)
     c = to_colormap(cmap, length(hist.weights))
     hist_αs = [Observable(1.) for i in c]
-    bincolor(αs...) = RGBAf0.(AbstractPlotting.color.(c), αs)
+    bincolor(αs...) = RGBAf0.(c, αs)
     colors = lift(bincolor, hist_αs...)
     hist_scene = plot(hist, color=colors)
     return hist_scene, hist_αs
@@ -103,9 +105,10 @@ end
 Get the index of the `selected_plot` in `scene`.
 """
 function get_series_idx(selected_plot, scene)
-    # TODO: There is probably a better or more efficient way of doing this.
-    plot_idx = findfirst(map(p->selected_plot === p, scene.plots))
-    plot_idx
+    # TODO: Find a less kacky way of doing this
+    # Note that the first index will be for the axis
+    plot_idx = findfirst(p->selected_plot === p, scene.plots)
+    return isnothing(plot_idx) ? nothing : plot_idx - 1
 end
 
 """
@@ -118,12 +121,18 @@ function setup_click(scene, idx=1)
     selection = Observable{Any}(0)
     on(scene.events.mousebuttons) do buttons
         if ispressed(scene, Mouse.left) && AbstractPlotting.is_mouseinside(scene)
-            plt, click_idx = AbstractPlotting.mouse_selection(scene)
+            plt, click_idx = mouse_selection(scene)
+            if toplevelparent(plt) isa BarPlot
+                click_idx = (click_idx + 1) ÷ 4
+            end
             selection[] = (plt, click_idx)[idx]
         end
     end
     return selection
 end
+
+toplevelparent(plt) = isa(plt.parent, Scene) ? plt : toplevelparent(plt.parent)
+toplevelparent(::Nothing) = nothing
 
 """
     bin_with_val(val, hist)
@@ -160,9 +169,9 @@ function select_series(scene, selected_plot, data_αs,
     series_idx = map(get_series_idx, selected_plot, scene)
     on(series_idx) do i
         if !isa(i, Nothing)
-            data_αs[i - 1][] = 1.0
-            change_α(data_αs, setdiff(axes(data_αs, 1), i - 1), α)
-            selected_bin = bin_with_val(data[i-1], hist)
+            data_αs[i][] = 1.0
+            change_α(data_αs, setdiff(axes(data_αs, 1), i), α)
+            selected_bin = bin_with_val(data[i], hist)
             hist_αs[selected_bin][] = 1.0
             change_α(hist_αs, setdiff(axes(hist_αs, 1), selected_bin), hα)
         else
