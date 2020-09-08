411060198a7edef1ac289854d0cd94e9ac199ee8
Datseris <datseris.george@gmail.com>
2020-09-08 22:15:11 +0200

    working orbit diagram (I think)

--- examples/orbitdiagram.jl
+++ examples/orbitdiagram.jl
@@ -8,7 +8,7 @@ systems = [(Systems.logistic(), 2.5, 4.0, "r", "logistic map"),
            (Systems.henon(), 0.8, 1.4, "a", "Hénon map"),
            (Systems.standardmap(), 0.0, 1.2, "k", "standard map")]
 
-ds, p_min, p_max, parname, t = systems[3]
+ds, p_min, p_max, parname, t = systems[2]
 t = "orbit diagram for the "*t
 
 oddata = interactive_orbitdiagram(ds, p_index, p_min, p_max, i;
