949d15ab75875f6cacd869067c8c7c5a618ceb31
Datseris <datseris.george@gmail.com>
2020-12-29 21:59:06 +0100

    clean up source using utils.jl

--- src/InteractiveChaos.jl
+++ src/InteractiveChaos.jl
@@ -3,100 +3,15 @@ module InteractiveChaos
 using AbstractPlotting, Observables
 using AbstractPlotting.MakieLayout
 
-"""
-    subscript(i::Int)
-Transform `i` to a string that has `i` as a subscript.
-"""
-function subscript(i::Int)
-    if i < 0
-        "₋"*subscript(-i)
-    elseif i == 1
-        "₁"
-    elseif i == 2
-        "₂"
-    elseif i == 3
-        "₃"
-    elseif i == 4
-        "₄"
-    elseif i == 5
-        "₅"
-    elseif i == 6
-        "₆"
-    elseif i == 7
-        "₇"
-    elseif i == 8
-        "₈"
-    elseif i == 9
-        "₉"
-    elseif i == 0
-        "₀"
-    else
-        join(subscript.(digits(i)))
-    end
-end
-
-"""
-    superscript(i::Int)
-Transform `i` to a string that has `i` as a superscript.
-"""
-function superscript(i::Int)
-    if i < 0
-        "⁻"*superscript(-i)
-    elseif i == 1
-        "¹"
-    elseif i == 2
-        "²"
-    elseif i == 3
-        "³"
-    elseif i == 4
-        "⁴"
-    elseif i == 5
-        "⁵"
-    elseif i == 6
-        "⁶"
-    elseif i == 7
-        "⁷"
-    elseif i == 8
-        "⁸"
-    elseif i == 9
-        "⁹"
-    elseif i == 0
-        "⁰"
-    else
-        join(superscript.(digits(i)))
-    end
-end
-
 const MARKER = Circle(Point2f0(0, 0), Float32(1)) # allows pixel size (zoom independent)
 const DEFAULT_BG = RGBf0(1.0, 1.0, 1.0)
 using AbstractPlotting: px
 
-"""
-    randomcolor(args...) = RGBAf0(0.9 .* (rand(), rand(), rand())..., 0.75)
-"""
-randomcolor(args...) = RGBAf0(0.9 .* (rand(), rand(), rand())..., 0.75)
-
-export subscript, superscript, randomcolor
-
 # JULIADYNAMICS_COLORS = to_color.(("#7a60bb", "#202020", "#1ba5aa"))
 JULIADYNAMICS_COLORS = to_color.(["#7d53e7", "#202020", "#17c7cd"])
+export JULIADYNAMICS_COLORS
 
-function colors_from_map(cmap, α, N)
-    N == 1 && return [RGBAf0(0, 0, 0, 1)]
-    cs = [RGBAf0(c.r, c.g, c.b, α) for c in AbstractPlotting.to_colormap(cmap, N)]
-end
-
-function pushupdate!(o::Observable, v)
-    push!(o[], v)
-    o[] = o[]
-    return o
-end
-
-function darken_color(c, f = 1.2)
-    tc = to_color(c)
-    return RGBAf0(c.r/f, c.g/f, c.b/f, c.alpha)
-end
-
+include("utils.jl")
 using Requires
 using Observables
 
---
+++ src/utils.jl
@@ -0,0 +1,86 @@
+export subscript, superscript, randomcolor
+
+"""
+    subscript(i::Int)
+Transform `i` to a string that has `i` as a subscript.
+"""
+function subscript(i::Int)
+    if i < 0
+        "₋"*subscript(-i)
+    elseif i == 1
+        "₁"
+    elseif i == 2
+        "₂"
+    elseif i == 3
+        "₃"
+    elseif i == 4
+        "₄"
+    elseif i == 5
+        "₅"
+    elseif i == 6
+        "₆"
+    elseif i == 7
+        "₇"
+    elseif i == 8
+        "₈"
+    elseif i == 9
+        "₉"
+    elseif i == 0
+        "₀"
+    else
+        join(subscript.(digits(i)))
+    end
+end
+
+"""
+    superscript(i::Int)
+Transform `i` to a string that has `i` as a superscript.
+"""
+function superscript(i::Int)
+    if i < 0
+        "⁻"*superscript(-i)
+    elseif i == 1
+        "¹"
+    elseif i == 2
+        "²"
+    elseif i == 3
+        "³"
+    elseif i == 4
+        "⁴"
+    elseif i == 5
+        "⁵"
+    elseif i == 6
+        "⁶"
+    elseif i == 7
+        "⁷"
+    elseif i == 8
+        "⁸"
+    elseif i == 9
+        "⁹"
+    elseif i == 0
+        "⁰"
+    else
+        join(superscript.(digits(i)))
+    end
+end
+
+"""
+    randomcolor(args...) = RGBAf0(0.9 .* (rand(), rand(), rand())..., 0.75)
+"""
+randomcolor(args...) = RGBAf0(0.9 .* (rand(), rand(), rand())..., 0.75)
+
+function colors_from_map(cmap, α, N)
+    N == 1 && return [RGBAf0(0, 0, 0, 1)]
+    cs = [RGBAf0(c.r, c.g, c.b, α) for c in AbstractPlotting.to_colormap(cmap, N)]
+end
+
+function pushupdate!(o::Observable, v)
+    push!(o[], v)
+    o[] = o[]
+    return o
+end
+
+function darken_color(c, f = 1.2)
+    tc = to_color(c)
+    return RGBAf0(c.r/f, c.g/f, c.b/f, c.alpha)
+end
