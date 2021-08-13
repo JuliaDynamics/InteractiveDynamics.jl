641f3d78ec1dde45b096a56586b3e753e77822fd
Frederik Banning <frederik.banning@ruhr-uni-bochum.de>
2021-08-13 14:59:30 +0200

    Add agent inspection on mouse hover for 2D

--- src/agents/abmplot.jl
+++ src/agents/abmplot.jl
@@ -19,3 +19,50 @@ function Makie.plot!(abmplot::ABMPlot{<:Tuple{Vector{Point2f0}, <:Agents.ABM}})
     )
     return abmplot
 end
+
+##########################################################################################
+# Agent inspection on mouse hover
+##########################################################################################
+
+function Makie.show_data(inspector::DataInspector, 
+            plot::ABMPlot{<:Tuple{Vector{Point2f0}, <:Agents.ABM}},
+            idx, ::Scatter)
+    a = inspector.plot.attributes
+    scene = Makie.parent_scene(plot)
+
+    proj_pos = Makie.shift_project(scene, plot, to_ndim(Point3f0, plot[1][][idx], 0))
+    Makie.update_tooltip_alignment!(inspector, proj_pos)
+    as = plot.as[]
+
+    # TODO: Generalise for use with both GridSpace and ContinuousSpace
+    pos = (plot[1][][idx].data[1], plot[1][][idx].data[2]) .|> Int
+    a._display_text[] = agent2string(plot.model[], pos)
+    a._bbox2D[] = FRect2D(proj_pos .- 0.5 .* as .- Vec2f0(5), Vec2f0(as) .+ Vec2f0(10))
+    a._px_bbox_visible[] = true
+    a._bbox_visible[] = false
+    a._visible[] = true
+
+    return true
+end
+
+"""
+Convert agent data into a string.
+
+Concatenate strings if there are multiple agents at given `pos`.
+"""
+function agent2string(model::Agents.ABM, pos::NTuple{2, Int})
+    ids = Agents.ids_in_position(pos, model)
+    s = ""
+    for id in ids
+        s *= agent2string(model[id]) * "\n"
+    end
+    return s
+end
+
+function agent2string(agent::A) where {A<:Agents.AbstractAgent}
+    agentstring = "▶ $(nameof(A))\n"
+    for field in fieldnames(A)
+        agentstring *= "$(field): $(getproperty(agent, field))\n"
+    end
+    return agentstring
+end
