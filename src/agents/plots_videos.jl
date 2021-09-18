9026494bb92e3b912feb727554f731537aaf001d
Datseris <datseris.george@gmail.com>
2021-09-18 18:34:17 +0200

    Make abm_play have default dummystep for model

--- src/agents/plots_videos.jl
+++ src/agents/plots_videos.jl
@@ -87,7 +87,7 @@ end
 ##########################################################################################
 
 """
-    abm_play(model, agent_step!, model_step!; kwargs...) → fig, abmstepper
+    abm_play(model, agent_step! [, model_step!]; kwargs...) → fig, abmstepper
 Launch an interactive application that plots an agent based model and can animate
 its evolution in real time. Requires `Agents`.
 
@@ -108,7 +108,7 @@ before the plot is updated, and "sleep" the `sleep()` time between updates.
 * `ac, am, as, scheduler, offset, aspect, scatterkwargs`: propagated to [`abm_plot`](@ref).
 * `spu = 1:100`: The values of the "spu" slider.
 """
-function abm_play(model, agent_step!, model_step!; spu = 1:100, kwargs...)
+function abm_play(model, agent_step!, model_step! = Agents.dummystep; spu = 1:100, kwargs...)
     fig = Figure(; resolution = (600, 700), backgroundcolor = DEFAULT_BG)
     ax = fig[1,1][1,1] = dimensionality(model) == 3 ? Axis3(fig) : Axis(fig)
     abmstepper = abm_init_stepper_and_plot!(ax, fig, model; kwargs...)
