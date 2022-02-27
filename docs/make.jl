6e12faf331efc45dd63afa3f1872702403190731
Datseris <datseris.george@gmail.com>
2022-02-27 14:02:27 +0100

    transform billiards into literate format

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,15 @@
 cd(@__DIR__)
 using Pkg
-Pkg.activate(@__DIR__)
 CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
+CI && Pkg.activate(@__DIR__)
 CI && Pkg.instantiate()
 
 using InteractiveDynamics
 using DynamicalSystems, DynamicalBilliards, Agents
 using Documenter
 using DocumenterTools: Themes
+using CairoMakie
 
-# %%
 # download the themes
 for file in ("juliadynamics-lightdefs.scss", "juliadynamics-darkdefs.scss", "juliadynamics-style.scss")
     download("https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/$file", joinpath(@__DIR__, file))
@@ -24,6 +24,18 @@ end
 Themes.compile(joinpath(@__DIR__, "juliadynamics-light.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-light.css"))
 Themes.compile(joinpath(@__DIR__, "juliadynamics-dark.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-dark.css"))
 
+
+# Use literate to transform files
+using Literate
+indir = joinpath(@__DIR__, "src")
+outdir = indir
+files = ("billiards.jl", )
+for file in files
+    Literate.markdown(joinpath(indir, file), outdir; credit = false)
+end
+
+# %%
+
 makedocs(
 modules=[InteractiveDynamics, DynamicalSystems, DynamicalBilliards, Agents],
 doctest=false,
@@ -37,13 +49,13 @@ format = Documenter.HTML(
     ),
 pages = [
     "Introduction" => "index.md",
-    "Trajectory Evolution" => "trajectory.md",
-    "Orbit Diagram" => "od.md",
-    "Cobweb Diagram" => "cobweb.md",
-    "Poincaré Surface of Section" => "psos.md",
-    "Brainscanning PSOS" => "brainscan.md",
+    # "Trajectory Evolution" => "trajectory.md",
+    # "Orbit Diagram" => "od.md",
+    # "Cobweb Diagram" => "cobweb.md",
+    # "Poincaré Surface of Section" => "psos.md",
+    # "Brainscanning PSOS" => "brainscan.md",
     "Billiards" => "billiards.md",
-    "Agent Based Models" => "agents.md",
+    # "Agent Based Models" => "agents.md",
     # "Trajectory Highlighter" => "highlight.md",
 ],
 )
--- docs/src/billiards.jl
+++ docs/src/billiards.jl
@@ -1,15 +1,14 @@
-# Visualizations and Animations for Billiards
+# # Visualizations and Animations for Billiards
 
-All plotting functionality of [DynamicalBilliards.jl](https://juliadynamics.github.io/DynamicalBilliards.jl/dev/) lies within a few well-defined functions from [InteractiveDynamics.jl](https://juliadynamics.github.io/InteractiveDynamics.jl/dev/) that use the [Makie](https://makie.juliaplots.org/stable/) ecosystem.
+# All plotting and animating for [DynamicalBilliards.jl](https://juliadynamics.github.io/DynamicalBilliards.jl/dev/) lies within a few well-defined functions from [InteractiveDynamics.jl](https://juliadynamics.github.io/InteractiveDynamics.jl/dev/) that use the [Makie](https://makie.juliaplots.org/stable/) ecosystem.
 
-- For static plotting, you can use the function [`bdplot`](@ref).
-- For interacting/animating, you can use the function [`bdplot_interactive`](@ref).
-  This function also allows you to create custom animations, see [Custom Billiards Animations](@ref).
-- For producing videos of time evolution of particles in a billiard, use [`bdplot_video`](@ref).
+# - For static plotting, you can use the function [`bdplot`](@ref).
+# - For interacting/animating, you can use the function [`bdplot_interactive`](@ref).
+#   This function also allows you to create custom animations, see [Custom Billiards Animations](@ref).
+# - For producing videos of time evolution of particles in a billiard, use [`bdplot_video`](@ref).
 
-## Plotting
-### Plotting an obstacle with keywords
-```@example BILLIARDS
+# ## Plotting
+# ### Plotting an obstacle with keywords
 using DynamicalBilliards, InteractiveDynamics, CairoMakie
 
 bd = billiard_sinai()
@@ -18,19 +17,18 @@ fig, ax = bdplot(bd[2])
 bdplot!(ax, bd[4]; color = "blue", linestyle = :dot, linewidth = 5.0)
 bdplot!(ax, bd[1]; color = "yellow", strokecolor = "black")
 fig
-```
-### Plotting a billiard
-```@example BILLIARDS
+
+# ### Plotting a billiard
+# %% #src
 using DynamicalBilliards, InteractiveDynamics, CairoMakie
 b = billiard_polygon(6, 1)
 a = Antidot([0.0,0.0], 0.5)
 bd = Billiard(b.obstacles..., a)
 fig, ax = bdplot(bd)
 fig
-```
 
-### Plotting some particle trajectories
-```@example BILLIARDS
+# ### Plotting some particle trajectories
+# %% #src
 using DynamicalBilliards, InteractiveDynamics, CairoMakie
 
 bd = billiard_hexagonal_sinai()
@@ -45,11 +43,10 @@ for (p, c) in zip([p1, p2], colors)
 end
 bdplot!(ax, [p1, p2]; colors, particle_size = 10, marker = markers)
 fig
-```
 
-### Periodic billiard plot
-Rectangle periodicity:
-```@example BILLIARDS
+# ### Periodic billiard plot
+# Rectangle periodicity:
+# %% #src
 using DynamicalBilliards, InteractiveDynamics, CairoMakie
 
 r = 0.25
@@ -63,10 +60,9 @@ fig, ax = bdplot(bd, extrema(xt)..., extrema(yt)...)
 lines!(ax, xt, yt)
 bdplot!(ax, p; velocity_size = 0.1)
 fig
-```
 
-Hexagonal periodicity:
-```@example BILLIARDS
+# Hexagonal periodicity:
+# %% #src
 using DynamicalBilliards, InteractiveDynamics, CairoMakie
 
 bd = billiard_hexagonal_sinai(0.3, 1.50; setting = "periodic")
@@ -81,74 +77,45 @@ fig, ax = bdplot(bd, extrema(xt)..., extrema(yt)...)
 lines!(ax, xt, yt)
 bdplot!(ax, p; velocity_size = 0.1)
 fig
-```
 
+# ## Interactive GUI
+# ```@dos
+# bdplot_interactive
+# ```
 
-## Interactive GUI
-```@dos
-bdplot_interactive
-```
+# ## Custom Billiards Animations
+# %% #src
+# To do custom animations you need to have a good idea of how Makie's animation system works.
+# Have a look [at this tutorial](https://www.youtube.com/watch?v=L-gyDvhjzGQ) if you are
+# not familiar yet.
 
-## Videos
-```@docs
-bdplot_video
-```
-Here is an example that changes plotting defaults to make an animation in the style of [3Blue1Brown](https://www.3blue1brown.com/).
-
-```@example BILLIARDS
-using DynamicalBilliards, InteractiveDynamics, CairoMakie
-
-BLUE = "#7BC3DC"
-BROWN = "#8D6238"
-colors = [BLUE, BROWN]
-# Overwrite default color of obstacles to white (to fit with black)
-InteractiveDynamics.obcolor(::Obstacle) = RGBf(1,1,1)
-bd = billiard_stadium(1, 1)
-ps = particlebeam(1.0, 0.6, 0, 1000, 0.001)
-
-bdplot_video(
-    "3b1billiard.mp4", bd, ps;
-    frames = 1200, backgroundcolor = :black, colors
-)
-```
-```@raw html
-<video width="auto" controls autoplay loop>
-<source src="../3b1billiard.mp4" type="video/mp4">
-</video>
-```
-
-## Custom Billiards Animations
-To do custom animations you need to have a good idea of how Makie's animation system works. Have a look [at this tutorial](https://www.youtube.com/watch?v=L-gyDvhjzGQ) if you are not familiar yet.
-
-Following the docstring of [`bdplot_interactive`](@ref) let's add a couple of new plots that animate some properties of the particles.
-We start with creating the billiard plot and obtaining the observables:
-```@example BILLIARDS
+# Following the docstring of [`bdplot_interactive`](@ref) let's add a couple of
+# new plots that animate some properties of the particles.
+# We start with creating the billiard plot and obtaining the observables:
 using DynamicalBilliards, InteractiveDynamics, CairoMakie
 
 bd = billiard_stadium(1, 1)
 N = 100
 ps = particlebeam(1.0, 0.6, 0, N, 0.001)
 fig, phs, chs = bdplot_interactive(bd, ps; playback_controls=false, resolution = (800, 800));
-```
-Then, we add some axis
-```@example BILLIARDS
+# Then, we add some axis
 layout = fig[2,1] = GridLayout()
 axd = Axis(layout[1,1]; ylabel = "log(⟨d⟩)", align = Outside())
 axs = Axis(layout[2,1]; ylabel = "std", xlabel = "time", align = Outside())
 hidexdecorations!(axd; grid = false)
 rowsize!(fig.layout, 1, Auto(2))
 fig
-```
-Our next step is to create new observables to plot in the new axis, by lifting `phs, chs`. Let's plot the distance between two particles and the std of the particle y position.
-```@example BILLIARDS
+# Our next step is to create new observables to plot in the new axis,
+# by lifting `phs, chs`. Let's plot the distance between two particles and the
+#  std of the particle y position.
 using Statistics: std
-# Define observables
+## Define observables
 d_p(phs) = log(sum(sqrt(sum(phs[1].p.pos .- phs[j].p.pos).^2) for j in 2:N)/N)
 std_p(phs) = std(p.p.pos[1] for p in phs)
 t = Observable([0.0]) # Time axis
 d = Observable([d_p(phs[])])
 s = Observable([std_p(phs[])])
-# Trigger observable updates
+## Trigger observable updates
 on(phs) do phs
     push!(t[], phs[1].T)
     push!(d[], d_p(phs))
@@ -156,13 +123,12 @@ on(phs) do phs
     notify.((t, d))
     autolimits!(axd); autolimits!(axs)
 end
-# Plot observables
+## Plot observables
 lines!(axd, t, d; color = JULIADYNAMICS_COLORS[1])
 lines!(axs, t, s; color = JULIADYNAMICS_COLORS[2])
 nothing
-```
-The figure hasn't changed yet of course, but after we step the animation, it does:
-```@example BILLIARDS
+
+# The figure hasn't changed yet of course, but after we step the animation, it does:
 dt = 0.001
 for j in 1:1000
     for i in 1:9
@@ -171,5 +137,35 @@ for j in 1:1000
     bdplot_animstep!(phs, chs, bd, dt; update = true)
 end
 fig
-```
-Of course, you can produce a video of this using Makie's `record` function.
+
+# Of course, you can produce a video of this using Makie's `record` function.
+
+# ## Video output
+# ```@docs
+# bdplot_video
+# ```
+# Here is an example that changes plotting defaults to make an animation in
+# the style of [3Blue1Brown](https://www.3blue1brown.com/).
+# %% #src
+using DynamicalBilliards, InteractiveDynamics, CairoMakie
+
+BLUE = "#7BC3DC"
+BROWN = "#8D6238"
+colors = [BLUE, BROWN]
+## Overwrite default color of obstacles to white (to fit with black background)
+InteractiveDynamics.obcolor(::Obstacle) = RGBf(1,1,1)
+bd = billiard_stadium(1, 1)
+ps = particlebeam(1.0, 0.6, 0, 200, 0.01)
+
+bdplot_video(
+    "3b1billiard.mp4", bd, ps;
+    frames = 120, colors, dt = 0.01, tail_length = 100,
+    backgroundcolor = :black,
+)
+nothing
+
+# ```@raw html
+# <video width="auto" controls autoplay loop>
+# <source src="../3b1billiard.mp4" type="video/mp4">
+# </video>
+# ```
