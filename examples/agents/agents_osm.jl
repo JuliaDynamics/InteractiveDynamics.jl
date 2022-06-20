00e7ce2e9ad10d875e2d076dc982e454109ea56e
Datseris <datseris.george@gmail.com>
2022-06-20 13:54:28 +0200

    fix agents all, inspection remaining

--- examples/agents/agents_osm.jl
+++ examples/agents/agents_osm.jl
@@ -5,15 +5,17 @@ using InteractiveDynamics
 zombie_model, zombie_step!, zombie_model_step! = Models.zombies()
 
 aczombie(agent) = agent.infected ? (:green, 0.9) : (:purple, 0.75)
-aszombie(agent) = agent.infected ? 10 : 8
+aszombie(agent) = agent.infected ? 15 : 12
 
 axiskwargs = (; title = "Zombie outbreak", backgroundcolor = "#f3f3f3")
 fig, ax, abmobs = abmplot(zombie_model; axiskwargs, ac = aczombie, as = aszombie)
 fig
 
 # %% with parameter sliders & time evolution
-fig, ax, abmobs = abmplot!(zombie_model; agent_step! = zombie_step!, model_step! = zombie_model_step!,
-    ac = aczombie, as = aszombie, axiskwargs, params = Dict(:dt => 0.1:0.01:0.2))
+fig, ax, abmobs = abmplot(zombie_model; agent_step! = zombie_step!, model_step! = zombie_model_step!,
+    ac = aczombie, as = aszombie, axiskwargs, params = Dict(:dt => 0.1:0.01:0.2),
+    enable_inspection = false,
+)
 fig
 
 
@@ -24,7 +26,8 @@ fig, abmobs = abmexploration(zombie_model;
     ac = aczombie, as = aszombie, params = Dict(:dt => 0.01:0.001:0.02),
     adata = [(:infected, count)], mdata = [zombie_share, :dt],
     alabels = ["Number of\nZombies"], mlabels = ["Zombification %", "distance per step"],
-    axiskwargs
+    axiskwargs,
+    enable_inspection = false,
 )
 
 fig
