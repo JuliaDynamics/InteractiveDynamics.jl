f255725e8f96ef66c56902957fb95ece9f820f18
Datseris <datseris.george@gmail.com>
2023-02-17 12:06:10 +0000

    specify scale explicitly

--- examples/agents/agents_flocking.jl
+++ examples/agents/agents_flocking.jl
@@ -70,7 +70,7 @@ model = init_flocking()
 const bird_polygon = Polygon(Point2f[(-0.5, -0.5), (1, 0), (-0.5, 0.5)])
 function bird_marker(b::Bird)
     φ = atan(b.vel[2], b.vel[1]) #+ π/2 + π
-    scale(rotate2D(bird_polygon, φ), 2)
+    InteractiveDynamics.scale(InteractiveDynamics.rotate2D(bird_polygon, φ), 2)
 end
 
 # simple plot
@@ -81,7 +81,6 @@ fig
 fig, ax, abmobs = abmplot(model;
     axis = (; title = "Flocking"),
     agent_step! = flocking_agent_step!,
-    model_step! = flocking_model_step!,
     am = bird_marker,
 )
 fig
