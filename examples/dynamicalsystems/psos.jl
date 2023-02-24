534c08347b7121e6f41cd74ee5b10906240a4e16
Datseris <datseris.george@gmail.com>
2023-02-24 15:24:19 +0000

    give up on Poincare map for now

--- examples/dynamicalsystems/psos.jl
+++ examples/dynamicalsystems/psos.jl
@@ -1,7 +1,8 @@
 using InteractiveDynamics, GLMakie, OrdinaryDiffEq, DynamicalSystems
-diffeq = (alg = Vern9(), abstol = 1e-9, reltol = 1e-9)
 
 hh = Systems.henonheiles()
+diffeq = (alg = Vern9(), abstol = 1e-9, reltol = 1e-9)
+hh = CoupledODEs(hh, diffeq)
 
 potential(x, y) = 0.5(x^2 + y^2) + (x^2*y - (y^3)/3)
 energy(x,y,px,py) = 0.5(px^2 + py^2) + potential(x,y)
@@ -21,7 +22,7 @@ plane = (1, 0.0) # first variable crossing 0
 # Interactive PSOS with random colors
 scene, state = interactive_poincaresos(
     hh, plane, (2, 4), complete;
-    labels = ("q₂" , "p₂"), diffeq
+    labels = ("q₂" , "p₂")
 );
 
 # %% Coloring points using a custom function
